64f56e09e599030a943609f70a6faa70167f39a5
Mamosebetsi Kekae <mkekae@gmail.com>
2022-02-28 15:26:35 +0200

    created a User model and migrated

---
+++ app/models.py
@@ -0,0 +1,16 @@
+from django.db import models
+from django.contrib.auth.models import  AbstractUser
+
+
+
+class User(AbstractUser):
+  first_name = models.CharField(max_length=255)
+  last_name = models.CharField(max_length=255)
+  email = models.EmailField(unique=True)
+  password = models.CharField(max_length=20)
+  username = None
+
+  
+
+  USERNAME_FIELD = 'email'
+  REQUIRED_FIELDS = []
